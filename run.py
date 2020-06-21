ea77bc4254433bb7602d8c20a2edf44ce2c3f788
Muneeb Sheikh <imuneebofficial@gmail.com>
2020-06-21 17:16:46 +0500

    setts feat

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 from project import app
 
 if __name__ == '__main__':
-	website_url = '127.0.0.1:5000'
+	website_url = '127.0.0.1:5001'
 	# website_url = '192.168.0.107:4996'
 	# website_url = 'localhost:4996'
 	# website_url = '10.133.128.149:5000'
